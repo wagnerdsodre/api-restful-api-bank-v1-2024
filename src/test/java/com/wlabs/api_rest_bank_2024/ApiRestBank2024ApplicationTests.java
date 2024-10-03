1b2cdc532e415e7774c4c3b676cfad636a697fb8
Wagner Sodré <wagnerdsodre@gmail.com>
2024-10-03 11:43:23 -0300

    Java RESTful API Bank simulation

---
+++ src/test/java/com/wlabs/api_rest_bank_2024/ApiRestBank2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlabs.api_rest_bank_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBank2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
