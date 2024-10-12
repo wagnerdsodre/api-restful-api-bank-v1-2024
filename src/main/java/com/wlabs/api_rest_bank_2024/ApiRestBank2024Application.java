63c92fb935604ea075ed66cdea417e392f068d9d
Wagner Sodré <wagnerdsodre@gmail.com>
2024-10-12 17:41:09 -0300

    Fix cors

--- src/main/java/com/wlabs/api_rest_bank_2024/ApiRestBank2024Application.java
+++ src/main/java/com/wlabs/api_rest_bank_2024/ApiRestBank2024Application.java
@@ -1,8 +1,11 @@
 package com.wlabs.api_rest_bank_2024;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = { @Server(url="/", description = "Default server url") })
 @SpringBootApplication
 public class ApiRestBank2024Application {
 
